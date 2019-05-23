8af0f38728e47f93c6d7d3bbf55d266b4fa5d734
Bane96 <bane_dragic96@hotmail.com>
2019-05-23 10:00:09 +0200

    initial

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import{ AngularFireModule } from 'angularfire2';
+import{AngularFireDatabaseModule}from 'angularfire2/database';
+import {environment} from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+
+    AngularFireDatabaseModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {  }
---
+++ src/environments/environment.ts
@@ -0,0 +1,28 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+
+ 
+    apiKey: "AIzaSyAD3tb8JB6v95-6dtnwuSgNA2-kdnoSlfk",
+    authDomain: "todo-dfd51.firebaseapp.com",
+    databaseURL: "https://todo-dfd51.firebaseio.com",
+    projectId: "todo-dfd51",
+    storageBucket: "todo-dfd51.appspot.com",
+    messagingSenderId: "852323276065",
+    //appId: "1:852323276065:web:53a5d12fcfa2e7d8"
+
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
